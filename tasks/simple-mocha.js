94ec1fe34567b98d39fe9e917d244edc571e5e05
Tyler Morgan <tylermorgan86@gmail.com>
2014-07-16 17:32:48 -0700

    messing with domains

--- tasks/simple-mocha.js
+++ tasks/simple-mocha.js
@@ -1,5 +1,4 @@
-/*
- * grunt-simple-mocha
+/* * grunt-simple-mocha
  * https://github.com/yaymukund/grunt-simple-mocha
  *
  * Copyright (c) 2012 Mukund Lakshman
@@ -10,25 +9,31 @@
 module.exports = function(grunt) {
 
   var path = require('path'),
-      Mocha = require('mocha');
+      Mocha = require('mocha'),
+      domain = require('domain');;
 
   grunt.registerMultiTask('simplemocha', 'Run tests with mocha', function() {
+    var d = domain.create();
+    d.on('error', function(err) {
+      console.log('error time')
+    });
+    d.run(function() {
+      var options = this.options(),
+          mocha_instance = new Mocha(options);
 
-    var options = this.options(),
-        mocha_instance = new Mocha(options);
+      this.filesSrc.forEach(mocha_instance.addFile.bind(mocha_instance));
 
-    this.filesSrc.forEach(mocha_instance.addFile.bind(mocha_instance));
+      // We will now run mocha asynchronously and receive number of errors in a
+      // callback, which we'll use to report the result of the async task by
+      // calling done() with the appropriate value to indicate whether an error
+      // occurred.
 
-    // We will now run mocha asynchronously and receive number of errors in a
-    // callback, which we'll use to report the result of the async task by
-    // calling done() with the appropriate value to indicate whether an error
-    // occurred.
+      var done = this.async();
 
-    var done = this.async();
-
-    mocha_instance.run(function(errCount) {
-      var withoutErrors = (errCount === 0);
-      done(options.force || withoutErrors);
+      mocha_instance.run(function(errCount) {
+        var withoutErrors = (errCount === 0);
+        done(withoutErrors);
+      });
     });
   });
 };
